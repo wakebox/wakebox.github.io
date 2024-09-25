56f80fc7f850defb8eec3446f89a8272565b6d7d
wakebox <wakebox@gmail.com>
2024-09-25 13:57:39 +0800

    Upload by PicGo

---
+++ data/myjobs.bat
@@ -0,0 +1,19 @@
+chcp 65001
+d:
+cd "D:\wilson_chu\Dropbox\工作\todo"
+git pull http://wilson:newsoft123@10.65.164.175/wilson/todo
+
+cd "D:\wilson_chu\Dropbox\工作\序號產生器_webserver"
+git pull   http://wilson:newsoft123@10.65.164.175/wilson/oasn
+
+cd "D:\wilson_chu\Dropbox\工作\newsoft\BisAll"
+git pull  http://wilson:newsoft123@10.65.164.175/wilson/BisAll
+
+cd "D:\wilson_chu\Dropbox\工作\newsoft\fubon\fubon"
+git pull  http://wilson:newsoft123@10.65.164.175/wilson/fubon
+
+cd "D:\wilson_chu\Dropbox\工作\newsoft\GD_PROJECT\gd"
+git pull  http://wilson:newsoft123@10.65.164.175/wilson/gd
+
+cd "D:\wilson_chu\Dropbox\工作\LT"
+git pull  http://wilson:newsoft123@10.65.164.175/wilson/LT
